5a400387677fdc497ce62055c4c6964099a241b6
Scott Miller <scott@millergeek.xyz>
2020-05-28 14:11:54 -0400

    remove commented test

--- main.tf
+++ main.tf
@@ -29,27 +29,3 @@ resource "aws_cloudformation_stack" "this" {
     { Name : "${var.name}-stack" }
   )
 }
-
-# resource "local_file" "foo" {
-#   content = templatefile("${path.module}/cloudformation.yml.tpl", {
-#     block_device_mappings = var.block_device_mappings
-#     description           = var.description
-#     name                  = var.name
-#     parent_image          = var.parent_image
-#     platform              = var.platform
-#     update                = var.update
-#     version               = var.recipe_version
-
-#     components = [
-#       for component in var.components : {
-#         ComponentArn = component
-#       }
-#     ]
-
-#     tags = merge(
-#       var.tags,
-#       { Name : var.name }
-#     )
-#   })
-#   filename = "${path.module}/test.yml"
-# }
