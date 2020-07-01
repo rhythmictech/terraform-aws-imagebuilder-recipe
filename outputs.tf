602510912555e9c69f98f1fb92efdf35874c32e9
Scott Miller <scott@millergeek.xyz>
2020-07-01 15:40:53 -0400

    arn depends_on

--- outputs.tf
+++ outputs.tf
@@ -9,4 +9,8 @@ locals {
 output "recipe_arn" {
   description = "ARN of the EC2 Image Builder Recipe"
   value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:image-recipe/${lower(var.name)}/${var.recipe_version}"
+
+  depends_on = [
+    aws_cloudformation_stack.this
+  ]
 }
