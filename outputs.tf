08ce726066806f154671dc63a79ee0f5b96d20ad
David Goodell <dgoodell@rhythmictech.com>
2020-12-03 20:46:18 -0500

    export a 'latest' recipe ARN using x.x.x

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,15 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
 }
 
+output "latest_recipe_arn" {
+  description = "ARN of the wildcard representing the latest EC2 Image Builder Recipe"
+  value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:image-recipe/${lower(var.name)}/x.x.x"
+
+  depends_on = [
+    aws_cloudformation_stack.this
+  ]
+}
+
 output "recipe_arn" {
   description = "ARN of the EC2 Image Builder Recipe"
   value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:image-recipe/${lower(var.name)}/${var.recipe_version}"
