ba24b20ac6e40207745fc6f4492936dd89cc32d3
David Goodell <dgoodell@rhythmictech.com>
2020-12-03 21:08:13 -0500

    fixing output

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,9 @@ locals {
   account_id = data.aws_caller_identity.current.account_id
 }
 
-output "latest_recipe_arn" {
+output "latest_image_arn" {
   description = "ARN of the wildcard representing the latest EC2 Image Builder Recipe"
-  value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:image-recipe/${lower(var.name)}/x.x.x"
+  value       = "arn:aws:imagebuilder:${local.region}:${local.account_id}:image/${lower(var.name)}/x.x.x"
 
   depends_on = [
     aws_cloudformation_stack.this
