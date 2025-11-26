37c7c8241488ebac8d155d014c9bde6507481de4
david goodell <dgoodell@rhythmictech.com>
2025-11-26 14:29:43 -0500

    fix for latest_image_arn output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "latest_image_arn" {
   description = "ARN of the wildcard representing the latest EC2 Image Builder Image"
-  value       = replace(aws_imagebuilder_image_recipe.this.arn, "///[0-9].[0-9].[0-9].$/", "/x.x.x")
+  value       = replace(aws_imagebuilder_image_recipe.this.arn, "//[0-9]+.[0-9]+.[0-9]+$/", "/x.x.x")
 }
 
 output "recipe_arn" {
